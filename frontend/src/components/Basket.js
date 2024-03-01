f71e40d34f1c823956009db899efa686609d9381
ericDIT <meyer20eric@gmail.com>
2024-03-01 12:37:05 +0100

    Basket structure added in frontend, Basket logic has to be implemented in backend next

--- frontend/src/components/Basket.js
+++ frontend/src/components/Basket.js
@@ -1,7 +1,33 @@
 import React from "react";
 import Navbar from './Navbar';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 const Basket = () => {
+    const [basket, setBasket] = useState([]);
+
+    useEffect(() => {
+      const userId = Cookies.get('userId');
+      fetchBasket(userId);
+    }, []);
+  
+    const fetchBasket = (userId) => {
+      // API-Anfrage an den Basket Microservice, um den Warenkorb abzurufen
+      // Verwenden Sie die Benutzer-ID aus dem Cookie, um den Warenkorb zuzuordnen
+      fetch(`/api/basket/${userId}`)
+        .then(response => response.json())
+        .then(data => setBasket(data));
+    };
+  
+    const addToBasket = (productId, quantity) => {
+      // Logik zum Hinzufügen eines Produkts zum Warenkorb
+      // Aktualisieren Sie den lokalen Zustand und senden Sie eine Anfrage an den Server, um den Warenkorb zu aktualisieren
+    };
+  
+    const removeFromBasket = (productId, quantity) => {
+      // Logik zum Entfernen eines Produkts aus dem Warenkorb
+      // Aktualisieren Sie den lokalen Zustand und senden Sie eine Anfrage an den Server, um den Warenkorb zu aktualisieren
+    };
     return (
         <div>
             <Navbar />
